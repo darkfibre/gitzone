87b150e71a5bf86169fc7c660c03161efb938687
Ryan Otis <ryan.otis@webtrends.com>
2012-01-26 13:33:43 -0800

    another push

--- gitzone.rb
+++ gitzone.rb
@@ -6,19 +6,25 @@ require 'yaml'
 
 res = Net::DNS::Resolver.new()
 
-zonelist = ReaderYAML.new('zonelist.yaml')
+#zonelist = ReaderYAML.new('config.yaml')
 
+#puts zonelist
 
+=begin
 zonelist.each do |zone,ns|
   res.nameserver=ns
   fullzone = res.axfr(zone)
   puts fullzone
 end
+=end
 
-
-class ReaderYAML
-  def initialize(file)
-    lines File.read(file)
-    @h = YAML::load(lines)
-  end
+def read_config
+  config = YAML.load_file("config.yaml")
+  config.each_key { |targetDirectory|
+    zonelist = config[targetDirectory]
+    puts "#{targetDirectory} will contain #{zonelist}"
+  }
 end
+
+read_config
+
