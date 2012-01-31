5813c5fcbea704b731fafa3faaa2aa641264dfb2
Ryan Otis <ryan.otis@webtrends.com>
2012-01-31 13:12:12 -0800

    Writes to files, commits changes to git.  No exception handling yet.

--- gitzone.rb
+++ gitzone.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'dnsruby'
 require 'yaml'
+require 'git'
 
 $res = Dnsruby::ZoneTransfer.new
 $res.transfer_type = Dnsruby::Types.AXFR
@@ -12,37 +13,58 @@ def read_config(cfgFile)
   
   configHash = {}
   
-  config.each_key do |location|
+  config.each_key do |locDir|
     zonepair = {}
     
-    config[location].each do |zone,ns|
+    config[locDir].each do |zone,ns|
       zonepair.store(zone, ns)
     end
     
-    configHash.store(location, zonepair)
+    configHash.store(locDir, zonepair)
   end
   
   return configHash
 end
 
+def write_zone(zoneAXFR,location)
+  if(zoneAXFR)
+    if File.exists?("#{location}")
+      if File.exists?("#{location}.tmp") then File.delete("#{location}.tmp") end
+      
+      zonefile = File.open("#{location}.tmp", "w")
+      
+      zoneAXFR.each do |rr|
+        zonefile.puts rr.inspect
+      end
+      
+      File.rename("#{location}.tmp", "#{location}")
+    else
+      puts "#{location} does not exist yet, aborting"
+    end
+  end
+end  
+
 zoneList = read_config("config.yaml")
 
-puts zoneList.inspect
-
-zoneList.each_key do |location|
-  puts "Working with #{location}"
+zoneList.each_key do |locDir|
+  puts "Working with #{locDir}"
   
-  zoneList[location].each do |zone,ns|
+  g = Git.init(working_dir = "#{locDir}")
+  g.status
+  
+  zoneList[locDir].each do |zone,ns|
     puts "Setting nameserver to #{ns}"
     $res.server = ns
     
-    puts "Performing AXFR of zone #{zone}"
-    fullzone = $res.transfer("#{zone}")
+    location = "#{locDir}/#{zone}"
     
-    if(fullzone)
-      fullzone.each do |rr|
-        puts rr.inspect
-      end
-    end
+    puts "Performing AXFR of zone #{zone}"
+    zoneAXFR = $res.transfer("#{zone}")
+
+    puts "Writing #{zone} to #{location}"
+    write_zone(zoneAXFR,location)
   end
+  
+  puts "Committing updates in #{locDir}"
+  g.commit_all("Script triggered update for #{zoneList}")
 end
